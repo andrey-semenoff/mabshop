37531a36064da9ff596dd6b2500ab895bf35bdbc
Andrey <andrey.semenoff@gmail.com>
2018-02-07 00:21:33 +0200

    Rebuild sidebar animation

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -41,6 +41,53 @@ $(function () {
       }
   	}
   });
+
+  $(document).scroll(function() {
+    var scroll_top = $(this).scrollTop(),
+        $menu = $('.menu'),
+        sidebar_top = $('.sidebar').offset().top;
+
+    if( scroll_top >= sidebar_top ) {
+      $menu.addClass('menu_fixed');
+    } else {
+       $menu.removeClass('menu_fixed');
+    }
+  });
+
+  if( $('.menu-item_active').length ) {
+    setPointerTop($('.menu-pointer'), $('.menu-item_active'), 'static');
+  }
+
+  // menu-pointer
+  $('.menu-link').on('mouseover', function() {
+    setPointerTop($('.menu-pointer'), $(this), 'dynamic');
+  });
+
+  $('.sidebar').on('mouseleave', function() {
+     $('.menu-pointer').css({
+      top: $('.menu-pointer').data('top') + 'px' 
+     }).removeClass('menu-pointer_dynamic');
+  });
+
 });
 
 
+///////////////////////////////
+// FUNCTIONS
+///////////////////////////////
+
+function setPointerTop($pointer, $target, state) {
+  var target_middle = $target.offset().top + $target.height() / 2,
+      sidebar_top = $('.menu').offset().top,
+      top = target_middle - sidebar_top - ($pointer.data('height') || $pointer.outerHeight()) / 2;
+  
+  if( state == 'dynamic' ) {
+    $pointer.addClass('menu-pointer_dynamic');
+  } else {
+    $pointer.data('top', top).data('height', $pointer.outerHeight());
+  }
+  
+  $pointer.show().css({
+    top: top + 'px'
+  });
+}
